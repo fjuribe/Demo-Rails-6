d8d7a9e38a0ffb72db9158ad452b1398e914e5d6
fjuribe <fjuribeui@gmail.com>
2020-06-05 17:04:03 -0400

    Examinando comportamiento de rutas

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+   
+
+   # forma generica para definir una ruta
    # get '/home', to: 'pages#home'
-   root 'pages#home'
+   # get '/home', => 'pages#home'
+     get '/pages/home'
+
+   #redirecciona automaticamente al cargar el sitio
+   #root 'pages#home'
 end
