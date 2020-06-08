3f344162e23f849fdd4e90094aa1106b8f449e94
fjuribe <fjuribeui@gmail.com>
2020-06-08 17:26:00 -0400

    Agregando recursos CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,40 @@
 Rails.application.routes.draw do
-  get 'author/index'
-  get 'author/create'
-  get 'author/delete'
+
+######## REST ##################################
+  root 'books#index'
+  
+
+  resources :books
+  
+  # resources books, only: [:index, :new, :create]
+  # resources authors, except: [:destroy]
+
+  # get "/books", to: "books#index"
+  # get "/books/new", to: "books#new"
+  # post "/books", to: "books#create"
+  # get "/books/:id", to: "books#show"
+  # get "/books/:id/edit", to: "books#edit"
+  # put "/books/:id", to: "books#update"
+  # patch "/books/:id", to: "books#update"
+  # delete "/books/:id", to: "books#destroy"
+
+######### ANTERIORES #######################################
+  # get 'author/index'
+  # get 'author/create'
+  # get 'author/delete'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
    
 
    # forma generica para definir una ruta
    # get '/home/:name',to:'pages#home'
-   get '/libros',to:'books#index', as: "home"
+   # get '/libros',to:'books#index', as: "home"
 
-   get '/prueba', to:'pages#prueba'
+   # get '/prueba', to:'pages#prueba'
 
-   get '/home' => 'pages#home'
+   # get '/home' => 'pages#home'
 
 
-   get '/autor', to: 'author#index' , as: 'author'
+   # get '/autor', to: 'author#index' , as: 'author'
 
    # es posible omitir el controlador
    # y el método siempre y cuando la ruta tenga la forma /<controlador>/<método>
