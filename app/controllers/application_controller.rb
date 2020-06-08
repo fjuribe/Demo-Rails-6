ca0df6bd7e0a171b34f804f00b7992ae90992f3d
fjuribe <fjuribeui@gmail.com>
2020-06-08 17:36:56 -0400

    Agregando filtros

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+
+	#before_action :log_request
+	before_action :log_request, only: [:new, :create]
+	before_action :log_request, except: [:index]
+
+	private
+
+	def log_request
+		puts "ha iniciado una peticion"
+	end
 end
